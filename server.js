2f60b2b23425fcbdb101d2c76971e80b635ec665
Anel <mazhitovaanelye@gmail.com>
2024-06-06 21:00:12 +0500

    fixed server js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Room = require('./models/room');
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://adamzat-client.onrender.com',
         methods: ['GET', 'POST'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
